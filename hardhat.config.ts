f7022495e1071133cd0a44f2e2c23c3d001395b3
EerieEight <80974420+EerieEight@users.noreply.github.com>
2021-09-29 18:12:43 -0400

    repo setup

---
+++ hardhat.config.ts
@@ -0,0 +1,76 @@
+import { task } from "hardhat/config"
+import "dotenv/config"
+import "ethers"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-solhint"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-abi-exporter"
+import "hardhat-deploy"
+import "solidity-coverage"
+import "hardhat-spdx-license-identifier"
+import { HardhatUserConfig } from "hardhat/types"
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const accounts = {
+  mnemonic: process.env.MNEMONIC || "test test test test test test test test test test test junk",
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config: HardhatUserConfig = {
+  defaultNetwork: "localhost",
+  mocha: {
+    timeout: 20000,
+  },
+  etherscan: {
+    apiKey: process.env.FTMSCAN_API_KEY
+  },
+  networks: {
+    localhost: {
+      live: false,
+      gasPrice: 20000000000,
+    },
+    fantom: {
+      url: "https://rpc.ftm.tools",
+      accounts,
+      chainId: 250,
+      gasPrice: 1000000000000,
+      saveDeployments: true,
+    },
+    "fantom-testnet": {
+      url: "https://rpc.testnet.fantom.network",
+      accounts,
+      chainId: 4002,
+      gasMultiplier: 2,
+    },
+  },
+  spdxLicenseIdentifier: {
+    overwrite: false,
+    runOnCompile: true,
+  },
+  solidity: {   
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      },
+    ],
+  },
+}
+export default config
